7adf3cd57e78282fcb0fcd19163c1ecf4dbcb2b8
solo <solohuckstep@gmail.com>
2023-02-23 02:17:30 -0500

    created manager test file and code

---
+++ Test/manager.test.js
@@ -0,0 +1,24 @@
+const manager = require("../lib/manager");
+
+
+
+test('getRole() return "Manager"', () => {
+    const testVal = "Manager";
+    const e = new manager("Foo", 1, "testVal@testing.com", 100);
+    expect(e.getRole()).toBe(testVal);
+});
+
+
+test('set office number with constructor argument', () => {
+    const testVal = 100;
+    const e = new manager("Foo", 1, "testVal@testing.com", testVal);
+    expect(e.officeNumber).toBe(testVal);
+});
+
+test('get office number with getOffice()', () => {
+    const testVal = 100;
+    const e = new manager("Foo", 1, "testVal@testing.com", testVal);
+    expect(e.getOfficeNumber()).toBe(testVal);
+});
+  
+  
