f1e8c9e210d625bbf355665a5c8a7f269aada030
solo <solohuckstep@gmail.com>
2023-02-23 02:21:24 -0500

    added manager lib file and code

---
+++ Lib/manager.js
@@ -0,0 +1,16 @@
+const employee = require('./employee');
+
+class manager extends employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+        this.role = 'Manager';
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+
+}
+
+module.exports = manager;
